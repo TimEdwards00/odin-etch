c8541c4bcf4d1697a4ce92411e6361629209057f
TimEdwards00 <91420130+TimEdwards00@users.noreply.github.com>
2023-08-02 15:13:54 +0100

    Completed project

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-const container = document.querySelector('#container-main');
-const size = 16;
-let pixelDimension = 32;
+const container = document.querySelector("#container-main");
+let size = 16;
+let pixelDimension = Math.floor(960 / size);
 
+function clear() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
 
-for (let i = 0; i < (size * size); i++) {
-    container.appendChild(document.createElement('div'))
+function genererateGrid() {
+  for (let i = 0; i < size ** 2; i++) {
+    container.appendChild(document.createElement("div"));
+  }
 
-};
+  const pixels = container.querySelectorAll("div");
 
-const pixels = container.querySelectorAll('div')
+  pixels.forEach((pixel) => {
+    pixel.classList.add("pixel");
+    pixel.setAttribute(
+      "style",
+      `height: ${pixelDimension}px; width: ${pixelDimension}px;`
+    );
 
-pixels.forEach((pixel) => {
-    pixel.classList.add('pixel')
-    pixel.setAttribute('style', `height: ${pixelDimension}px; width: ${pixelDimension}px;`); 
-    
-    pixel.addEventListener('mouseover', () => {
-        pixel.classList.toggle('active');
-    })
-})
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.toggle("active");
+    });
+  });
+}
+
+const setSizeButton = document.querySelector("#set-size");
+
+setSizeButton.addEventListener("click", () => {
+  size = prompt("Integer between 1 and 96");
+  pixelDimension = Math.floor(960 / size);
+  clear();
+  genererateGrid();
+});
+
+const eraseButton = document.querySelector("#erase");
+
+eraseButton.addEventListener("click", () =>{
+    clear();
+    genererateGrid();
+}
+)
+
+genererateGrid();
